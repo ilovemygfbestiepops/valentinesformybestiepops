1e503e0b6588b93183f16c8c44f10fb8defc4464
ilovemygfbestiepops <selvachetty23@gmail.com>
2025-10-26 18:29:32 +0200

    Fix layout and styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,20 @@
 import "./globals.css";
 
-
 export const metadata = {
-  title: "Valentines 2024",
-  description: "Avent sent you a letter!",
-  metadataBase: new URL('https://aventchiu.com/valentines'),
+  title: "Valentines 2026",
+  description: "Shaylin sent you a letter! 💌",
   openGraph: {
-    images: 'heart.png'
-  }
+    images: ["heart.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-pink-100 flex items-center justify-center min-h-screen">
+        {children}
+      </body>
     </html>
   );
 }
+
